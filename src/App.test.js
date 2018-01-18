71ed3c54310791a815d7cb3238b56c9d6f4abc15
julien jamet <jamet.julien@gmail.com>
2018-01-18 16:48:21 +0100

    add enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+
+
 
 it('renders without crashing', () => {
+
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+
+});
+
+it('addition', () => {
+  
 });
